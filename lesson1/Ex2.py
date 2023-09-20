5e5c32c2435bcd8d2b206b817463a19c471411e9
artyomklg <artem0801200269@gmail.com>
2023-09-20 20:39:43 +0300

    Ex2

---
+++ lesson1/Ex2.py
@@ -0,0 +1,20 @@
+def main() -> None:
+    try:
+        num1: int = int(input("Ввести произвольное число: "))
+        num2: int = int(input("Ввести пограничное число: "))
+    except ValueError:
+        print("Неверный формат ввода")
+        return
+
+    if num1 < num2:
+        print("1-ое число меньше пограничного")
+    else:
+        to_print: str = "1-ое число больше пограничного"
+
+        if num1 > num2 * 3:
+            to_print = f"{to_print}, причем более чем в 3 раза"
+        print(to_print)
+
+
+if __name__ == "__main__":
+    main()
